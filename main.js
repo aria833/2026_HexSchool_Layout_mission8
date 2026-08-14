5b96200595cc36a6bafdc0a492bdc770e1064595
Aria <aria.mltang@gmail.com>
2026-08-15 01:23:52 +0800

    add calculation section

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
-import './assets/scss/all.scss';
-import 'bootstrap/dist/js/bootstrap.min.js';
+import "./assets/scss/all.scss";
+import "bootstrap/dist/js/bootstrap.min.js";
 
-console.log('Hello world');
+console.log("Hello world");
+
+//精選房源輪播
+const swiper = new Swiper(".swiper", {
+  // 分頁、左右箭頭、滾動條若有使用則必需設定
+  // 分頁
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  // 左右箭頭
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  // 滾動條
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
