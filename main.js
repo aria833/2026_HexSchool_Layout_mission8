2c9a0cc763b110ce9036414af669838ec1132165
Aria <aria.mltang@gmail.com>
2026-08-16 01:35:38 +0800

    add house page

--- main.js
+++ main.js
@@ -40,3 +40,14 @@ const observer = new IntersectionObserver(
 );
 
 observer.observe(contactArea);
+
+//圖片燈箱效果
+import PhotoSwipeLightbox from "https://cdn.jsdelivr.net/npm/photoswipe@5.4.4/dist/photoswipe-lightbox.esm.min.js";
+import PhotoSwipe from "https://cdn.jsdelivr.net/npm/photoswipe@5.4.4/dist/photoswipe.esm.min.js";
+
+const lightbox = new PhotoSwipeLightbox({
+  gallery: "#image-gallery",
+  children: "a",
+  pswpModule: PhotoSwipe,
+});
+lightbox.init();
